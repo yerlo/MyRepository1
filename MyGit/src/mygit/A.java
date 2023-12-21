070d9e4a538b548eccb9796a2f92dd25fcdae6a7
서예은 <@192.168.200.124>
2023-12-22 03:35:10 +0900

    A랑 C 수정

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -4,6 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("this is A.java");
 		System.out.println("okat master~");
+		System.out.println("devC A.java 수정");
 	}
 
 }
--- MyGit/src/mygit/C.java
+++ MyGit/src/mygit/C.java
@@ -3,6 +3,7 @@ package mygit;
 public class C {
 	public static void main(String[] args) {
 		System.out.println("this is C.java");
+		System.out.println("devC C.java 수정");
 	}
 
 }
